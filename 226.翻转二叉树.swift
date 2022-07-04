dd9124f146886df2963fe8c1e87847b95a4e0e3f
veraposeidon <veraposeidon@gmail.com>
2022-07-04 23:07:09 +0800

    feat: weekly leetcode
    HOB-330

---
+++ 226.翻转二叉树.swift
@@ -0,0 +1,35 @@
+// swiftlint:disable trailing_newline
+/*
+ * @lc app=leetcode.cn id=226 lang=swift
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        if root == nil { return nil }
+        let left = invertTree(root?.left)
+        let right = invertTree(root?.right)
+        root?.left = right
+        root?.right = left
+        return root
+    }
+}
+// @lc code=end
+
